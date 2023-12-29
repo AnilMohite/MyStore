9f94c887692ca9929c168e4bcfdb723356ce3ca7
AnilMohite <mohiteanil22@gmail.com>
2023-12-29 20:03:15 +0530

    create new app for category and create model

--- MyStore/urls.py
+++ MyStore/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    # path("category/", include("category.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ category/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.CharField(max_length=255, blank=True)
+    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
