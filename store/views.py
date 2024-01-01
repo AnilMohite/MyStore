7bb18e45c9aef70813cf27bedaf602b6fd190592
AnilMohite <mohiteanil22@gmail.com>
2024-01-01 12:46:09 +0530

    worked on cart and product deatil page fix some code

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponse
 
 def store(request, category_slug=None):
     if category_slug != None:
@@ -18,9 +21,11 @@ def store(request, category_slug=None):
 def product_detail(request,category_slug=None,product_slug=None):
     try:
         single_product = Product.objects.get(category__slug = category_slug, slug = product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product = single_product).exists()
     except Exception as e:
         raise e
     context = {
-        'product':single_product
+        'product' : single_product,
+        'in_cart' : in_cart
     }
     return render(request, 'store/product-detail.html', context)
