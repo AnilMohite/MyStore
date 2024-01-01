f9f94253685e71a756804741eaeeda0d743a20aa
AnilMohite <mohiteanil22@gmail.com>
2024-01-01 10:34:16 +0530

    cart template setup and cart and cart item model created

---
+++ carts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from store.models import Product
+
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+    
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product
+
---
+++ carts/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def cart(request):
+    return render(request, 'store/cart.html')
