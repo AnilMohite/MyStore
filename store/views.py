dd3a333587bb5f0893c3323a4072c0b5fe6cb363
AnilMohite <mohiteanil22@gmail.com>
2024-01-02 09:47:23 +0530

    store page pagination added

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
-from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 def store(request, category_slug=None):
     if category_slug != None:
@@ -11,9 +11,13 @@ def store(request, category_slug=None):
         products = Product.objects.filter(is_available = True, category = category)
     else:
         products = Product.objects.filter(is_available = True)
+
+    paginator = Paginator(products,1)
+    page = request.GET.get('page')
+    page_products = paginator.get_page(page)
     total_products = products.count()
     context = {
-        'products':products,
+        'products':page_products,
         'total_products':total_products
     }
     return render(request, 'store/store.html', context)
