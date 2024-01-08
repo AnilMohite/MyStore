46268e57c1bbf1f9e4c29affa1a5efcc10817c4c
AnilMohite <mohiteanil22@gmail.com>
2024-01-08 09:45:18 +0530

    created register and login paths

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def register(request):
+    return HttpResponse('register')
+
+def login(request):
+    return HttpResponse('login')
