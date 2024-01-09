3284805af2c2a34c7f70d9b4e14881468ad1b448
AnilMohite <mohiteanil22@gmail.com>
2024-01-09 10:17:36 +0530

    registration logic added

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,3 +17,10 @@ class RegistrationForm(forms.ModelForm):
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm,self).clean()
+        password = cleaned_data.get('password') 
+        confirm_password = cleaned_data.get('confirm_password') 
+        if password != confirm_password:
+            raise forms.ValidationError('Password does not match')
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, HttpResponse
 from .forms import RegistrationForm
+from .models import Account
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+
+            user = Account.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                password = password
+            )
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
     context = {
         'form':form
     }
