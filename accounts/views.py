42ba43ebf4302bbbb59ddc477f58f8765848eb63
AnilMohite <mohiteanil22@gmail.com>
2024-01-13 12:24:16 +0530

    user email varification decode

--- accounts/views.py
+++ accounts/views.py
@@ -81,4 +81,18 @@ def logout(request):
 
 
 def activate(request, uidb64, token):
-    return HttpResponse('Ok')
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, "Congratulations your account is activated!")
+        return redirect('login')
+    else:
+        messages.error(register, "Invalid activation link")
+        return redirect('register')
+    
