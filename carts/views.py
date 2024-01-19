75fdbf71897db68483d73992d0b8d5ea4b586657
AnilMohite <mohiteanil22@gmail.com>
2024-01-19 20:32:50 +0530

    working on product variation like size and color

--- carts/views.py
+++ carts/views.py
@@ -9,6 +9,9 @@ def _cart_id(request):
     return cart
 
 def add_cart(request, product_id):
+    color = request.GET.get('color')
+    size = request.GET.get('size')
+
     product = Product.objects.get(id = product_id)
     try:
         cart =  Cart.objects.get(cart_id = _cart_id(request))
