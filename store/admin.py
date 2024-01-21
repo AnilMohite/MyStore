3bd2bd7005ec688e05b4e38eaee5da557d64672e
AnilMohite <mohiteanil22@gmail.com>
2024-01-21 19:56:08 +0530

    register variation in admin model

--- store/admin.py
+++ store/admin.py
@@ -6,4 +6,8 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('product_name',)}
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Variation)
+
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_filter = ('variation_category',)
+admin.site.register(Variation, VariationAdmin)
