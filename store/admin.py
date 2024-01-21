068b298519f82003fd5c8ee9e1e37099310eaa5d
AnilMohite <mohiteanil22@gmail.com>
2024-01-21 19:58:28 +0530

    update

--- store/admin.py
+++ store/admin.py
@@ -4,10 +4,9 @@ from .models import Product, Variation
 class ProductAdmin(admin.ModelAdmin):    
     list_display = ('product_name','price','stock','category','is_available','modified_date')
     prepopulated_fields = {'slug':('product_name',)}
-
 admin.site.register(Product, ProductAdmin)
 
+@admin.register(Variation)
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active')
     list_filter = ('variation_category',)
-admin.site.register(Variation, VariationAdmin)
