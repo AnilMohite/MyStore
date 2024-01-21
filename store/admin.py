d5e79dcccb3b983e94c8113e17356f08412d93b4
AnilMohite <mohiteanil22@gmail.com>
2024-01-21 19:49:37 +0530

    product variation model created

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 class ProductAdmin(admin.ModelAdmin):    
     list_display = ('product_name','price','stock','category','is_available','modified_date')
     prepopulated_fields = {'slug':('product_name',)}
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation)
--- store/models.py
+++ store/models.py
@@ -19,3 +19,18 @@ class Product(models.Model):
     
     def get_url(self):
         return reverse('product_detail',args=[self.category.slug, self.slug])
+
+variation_category_choices = (
+    ('color','color'),
+    ('size','size'),
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    variation_category = models.CharField(max_length = 100, choices = variation_category_choices)
+    variation_value = models.CharField(max_length = 100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.product
