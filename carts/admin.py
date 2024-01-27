2b76bf1ce38cd6e232e3e573a32beee1c3283678
AnilMohite <mohiteanil22@gmail.com>
2024-01-27 10:26:00 +0530

    cart and cart items defined in admin

--- carts/admin.py
+++ carts/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Cart, CartItem
 
-admin.site.register(Cart)
-admin.site.register(CartItem)
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'created_date')
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'is_active')
